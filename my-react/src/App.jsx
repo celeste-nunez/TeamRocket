823c5a2114c5ea5e55c6f8f5a3d9f3d3b95e75eb
Isaiah Carrin <104584679+iCarrin@users.noreply.github.com>
2025-02-20 21:54:44 -0700

    added the "check off the habit for the day" module. No JavaScript yet

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import './App.css';
+import Check_habit from './Check_habit'
 import NavBar from './Components/NavBar'
 
 function App() {
 
   return (
-    <NavBar/>
+    <>
+      <NavBar/>
+      <h3>Hi, I'm Paul!</h3>
+      <Check_habit/>
+    </>
   );
 };
 
