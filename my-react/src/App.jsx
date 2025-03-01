004509d37eadde283e437bf0c9899baa6c482791
Isaiah Carrin <104584679+iCarrin@users.noreply.github.com>
2025-03-01 11:54:37 -0700

    manually changed app to be current brach compliant

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,27 +1,17 @@
-<<<<<<< HEAD
-import './App.css'
-import MyButton from './Components/MyButton';
-import NavBar from './Components/NavBar';
-
-=======
 import './App.css';
 // import Check_habit from './Check_habit'
 import NavBar from './Components/NavBar'
 import Habitling from './Habitling'
->>>>>>> ec4b5e6ca7d6760f8b72d43ef0b9af05802bcff2
+import HabitCreate from './Components/HabitCreate'
 function App() {
+
   return (
     <>
-<<<<<<< HEAD
-      <MyButton text="Please Work" action={console.log("pressed")} />
-      <NavBar />
-=======
       <NavBar/>
       <Habitling/>
->>>>>>> ec4b5e6ca7d6760f8b72d43ef0b9af05802bcff2
+      <HabitCreate/>
     </>
   );
-
-}
+};
 
 export default App
