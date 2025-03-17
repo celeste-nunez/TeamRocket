fc5a6104f0fa819e75f8b7149b6dc3fe1ccf7ac2
Isaiah Carrin <104584679+iCarrin@users.noreply.github.com>
2025-03-17 15:36:14 -0600

    Multi page  isaiah (#33)
    * I deleted the check box because I was the one that made it and we don't need it any more
    
    * Added multi page functionality, and put all pages into React components
    
    * deleted or moved all old files

--- my-react/src/Components/NavBar.jsx
+++ my-react/src/Components/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router-dom"; 
+import './NavBar.css';
 
 const NavBar = () => {
     return (
         <footer>
             <h1 className="nav-head">Habitlings</h1>
             <nav className="nav-menu">
-                <button className="nav-opt">create habitling</button>
-                <button className="nav-opt">view progress</button>
-                <button className="nav-opt">manage tasks</button>
+                <Link to="/" className="nav-opt">Home Page</Link>
+                <Link to="/HabitlingZoo" className="nav-opt">Habitling Zoo</Link>
+                <Link to="/AboutUs" className="nav-opt">About the Creators</Link>
             </nav>
         </footer>
     );
