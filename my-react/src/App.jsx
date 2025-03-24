bf9020786381e3611d7208674bf16c8d97e21da9
Isaiah Carrin <104584679+iCarrin@users.noreply.github.com>
2025-03-24 17:33:09 -0600

    added note about where I'm at

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -30,3 +30,24 @@ function App() {
 }
 
 export default App
+
+
+//What has been done
+//I've changed the app and data files to export functions rather than exporting HTML. 
+// I've started working on porting habitlings in and out. So far I only have a useEffect function that waits for the authentication state to change
+//when It does it see if there's a user. If there is a user still (new or old) it loades the habitlings, if there is no use it gives and empty list of habitlings.
+//all of the meat of habitlings is stored in "habits" 
+// (i.e. const [habits, setHabits] = useState([]);
+        //        ^ this guy right here
+//       const currentDayIndex = new Date().getDay();
+//       const [user, setUser] = useState(null);
+// )
+
+//so adding to or taking away from that list is how we change what appears on screen. Look to the day clicking functionality to see how to take one out of the list and change it
+
+
+//what still needs to be done with firebase:
+//we need to add a send habitling functionality to habitling. Theres a note in the "addHabit" function at the bottom about that
+//we need to add updateaHabit functionality. 
+//along those lines we have to go back into Data.jsx and change all of the code that sent and loaded messages to sending and loading habitlings. 
+//I've changed the names but not the code.
