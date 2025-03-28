fa24a56779465aafdcb1f1cf9435a2424f19f3ba
Holden Turman <holden.turman1@gmail.com>
2025-03-28 16:09:42 -0600

    Trying to automate sprite changes in create form. Also had to install vite 6.2.0 again

--- my-react/src/Components/Habitling.jsx
+++ my-react/src/Components/Habitling.jsx
@@ -54,7 +54,7 @@ const Habitling = () => {
       frequency: `Start: ${newHabit.startDate} - End: ${newHabit.endDate}`,
       currentStreak: 0,
       bestStreak: 0,
-      image: {Sprite},
+      image: <Sprite sprite={newHabit.sprite} />,
       completion: Array(7).fill(false),
       lastUpdatedWeek: new Date().getWeek(), // Track the current week
     };
