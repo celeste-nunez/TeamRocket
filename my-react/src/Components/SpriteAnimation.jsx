c9609aa826bbe4da165c0fa1cc39763d7b029ad1
Hannah Crenshaw <66535303+PinkCheetah1@users.noreply.github.com>
2025-04-02 15:19:42 -0600

    Updated Sprites (personal sprites)
    Added 7 sprites and their respective JSON. Changed SpriteAnimation.jsx to import from the updated sprite sheet. Old sprite sheet is still in folder.
    UNTESTED. Please open this and check the sprites before squashing! <3
    
    Co-authored-by: Isaiah Carrin <104584679+iCarrin@users.noreply.github.com>

--- my-react/src/Components/SpriteAnimation.jsx
+++ my-react/src/Components/SpriteAnimation.jsx
@@ -7,21 +7,21 @@ function AnimateSprite({sprite = { row: 0, column: 0 , frames: 1, time: "1s"} })
   const spriteRef = useRef(null);
  
  
-//   Runs whenever sprite changes ([sprite] dependency array).
-  useEffect(() => {
-    if (sprite && spriteRef.current) {
-      spriteRef.current.style.setProperty("--row", sprite.row);
-      spriteRef.current.style.setProperty("--column", sprite.column);
-      spriteRef.current.style.setProperty("--frames", sprite.frames);
-      spriteRef.current.style.setProperty("--time", sprite.time);
-    }
-  }, [sprite]);
- 
-  return (
-    <div ref={spriteRef} className="sprite size">
-      <img className="sprite-sheet pixelart down" src={SpriteSheet} alt="Selected Habitling" />
-    </div>
-  );
-}
+  //   Runs whenever sprite changes ([sprite] dependency array).
+    useEffect(() => {
+      if (sprite && spriteRef.current) {
+        spriteRef.current.style.setProperty("--row", sprite.row);
+        spriteRef.current.style.setProperty("--column", sprite.column);
+        spriteRef.current.style.setProperty("--fames", sprite.frames);
+        spriteRef.current.style.setProperty("--time", sprite.time);
+      }
+    }, [sprite]);
+
+    return (
+      <div ref={spriteRef} className="sprite size">
+        <img className="sprite-sheet pixelart down" src={SpriteSheet} alt="Selected Habitling" />
+      </div>
+    );
+  }
  
 export default AnimateSprite;
