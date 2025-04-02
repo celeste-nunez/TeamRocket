2f9cfd2205d0f0e947c06676dbcc7dd611d037bb
Isaiah Carrin <104584679+iCarrin@users.noreply.github.com>
2025-04-02 15:08:32 -0600

    Create form pet selection 2.0 (#51)
    * Updated sprite animations and habit create/habitling form sprites
    
    * Everything should be working. Added hannah's jason file and 8 frame animation
    
    * alphabatized the sprites
    
    ---------
    
    Co-authored-by: Holden Turman <holden.turman1@gmail.com>

--- my-react/src/Components/SpriteAnimation.jsx
+++ my-react/src/Components/SpriteAnimation.jsx
@@ -1,14 +1,27 @@
-import './SpriteAnimation.css'
-import SpriteSheet from '../assets/spritesheet_5.png';
-// import Shadow from "https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacterShadow.png";
-function AnimateSprite(){
-
-
-    return(
-        <div className="sprite size">
-            {/* <img className="sprite-shadow pixelart size" src={Shadow} alt="Sprite shadow" /> */}
-            <img className="sprite-sheet pixelart down" src={SpriteSheet} alt="This is the image with all potential habitlings on it" />
-        </div>
-    );
+import "./SpriteAnimation.css";
+import SpriteSheet from "../assets/spritesheet_5.png";
+import { useEffect, useRef } from "react";
+ 
+ 
+function AnimateSprite({sprite = { row: 0, column: 0 , frames: 1, time: "1s"} }) { // Fallback default value
+  const spriteRef = useRef(null);
+ 
+ 
+//   Runs whenever sprite changes ([sprite] dependency array).
+  useEffect(() => {
+    if (sprite && spriteRef.current) {
+      spriteRef.current.style.setProperty("--row", sprite.row);
+      spriteRef.current.style.setProperty("--column", sprite.column);
+      spriteRef.current.style.setProperty("--frames", sprite.frames);
+      spriteRef.current.style.setProperty("--time", sprite.time);
+    }
+  }, [sprite]);
+ 
+  return (
+    <div ref={spriteRef} className="sprite size">
+      <img className="sprite-sheet pixelart down" src={SpriteSheet} alt="Selected Habitling" />
+    </div>
+  );
 }
+ 
 export default AnimateSprite;
